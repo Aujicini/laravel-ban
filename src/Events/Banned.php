7f0221ac876911f3b2aa39cb6754d2cffa490570
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2020-05-30 18:27:03 -0400

    Add a banned event.

---
+++ src/Events/Banned.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oxuwazet\Laravel\Ban\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Banned
+{
+    use Dispatchable, SerializesModels;
+
+    /** @var mixed $user The user that was banned. */
+    public $sender;
+
+    /** @var array $data The ban data associated. */
+    public $data;
+
+    /**
+     * Create a new banned event.
+     *
+     * @param mixed $user The user that was banned.
+     * @param array $data The ban data associated.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct($user, $data = [])
+    {
+        $this->user = $user;
+        $this->data = $data;
+    }
+}
