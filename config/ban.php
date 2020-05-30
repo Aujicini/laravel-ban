acf7024ae009e9e50ff919cc3f68f9900c7962b9
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2020-05-30 18:01:50 -0400

    Add publishable config file.

---
+++ config/ban.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Namespace Support
+    |--------------------------------------------------------------------------
+    |
+    | Once enabled you can ban a user to a certain defined namespace. For example,
+    | I could do `$user->ban($options, 'chat')`. This can be used to chat ban a user
+    | or you could do `$user->ban($options, 'site')`. Which can mean the user is banned
+    | from the site. We enable this by default.
+    |
+    */
+
+    'namespace_support' => true,
+
+];
