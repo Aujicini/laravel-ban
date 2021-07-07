2d61a825b1e3a23b078cb75ff2648f7ec08e1a14
Nicholas English <oxuwazet@gmail.com>
2021-07-07 15:14:31 -0500

    Add `ban` model.

---
+++ src/Models/Ban.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Omatamix\LaravelBan\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ban extends Model
+{
+    /** @var array $casts The attributes that should be cast. */
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    /** @var array $fillable The attributes that are mass assignable. */
+    protected $fillable = [
+        'user_id',
+        'reason',
+        'expires_at',
+    ];
+
+    /**
+     * Get the relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo Returns the relation.
+     */
+    public function bannable()
+    {
+        return $this->morphTo();
+    }
+}
